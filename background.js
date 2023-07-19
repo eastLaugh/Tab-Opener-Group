b55e89cbd0b95e788e7d10e7b16d4c2dd38fbd0c
不笑 <east_laugh@qq.com>
2023-07-19 20:16:27 +0800

    引入Typescript

--- background.js
+++ background.js
@@ -1,43 +1,52 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { on, off } from './constants.js';
-var state = on
-
+var state = on;
 chrome.runtime.onInstalled.addListener(function () {
-    setBadge()
-})
-
-chrome.action.onClicked.addListener(async (tab) => {
-    const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
+    setBadge();
+});
+chrome.action.onClicked.addListener((tab) => __awaiter(void 0, void 0, void 0, function* () {
+    const prevState = yield chrome.action.getBadgeText({ tabId: tab.id });
     console.log(prevState);
     state = prevState === on ? off : on;
-    setBadge()
-})
-
+    setBadge();
+}));
 function setBadge() {
-    console.log(state)
+    console.log(state);
     chrome.action.setBadgeText({ text: state });
     if (state === on) {
-        chrome.action.setBadgeBackgroundColor({ color: "lightblue" })
-    } else if (state === off) {
-        chrome.action.setBadgeBackgroundColor({ color: "#D3D3D3" })
+        chrome.action.setBadgeBackgroundColor({ color: "lightblue" });
+    }
+    else if (state === off) {
+        chrome.action.setBadgeBackgroundColor({ color: "#D3D3D3" });
     }
 }
-
 chrome.webNavigation.onCommitted.addListener(function (details) {
     if (state === on) {
+        console.log(details.transitionType);
         if (details.transitionType === "link") {
             chrome.tabs.get(details.tabId, function (tab) {
                 if (tab.openerTabId !== undefined) {
                     chrome.tabs.get(tab.openerTabId, function (openerTab) {
                         if (openerTab.groupId !== -1) {
-                            chrome.tabs.group({ tabIds: [tab.id, tab.openerTabId], groupId: openerTab.groupId })
-                        } else {
-                            chrome.tabs.group({ tabIds: [tab.id, tab.openerTabId] })
+                            chrome.tabs.group({ tabIds: [tab.id, tab.openerTabId], groupId: openerTab.groupId });
                         }
-                    })
+                        else {
+                            chrome.tabs.group({ tabIds: [tab.id, tab.openerTabId] });
+                        }
+                    });
                 }
-            })
-        } else if (details.transitionType === "auto_bookmark" || details.transitionType === "typed" || details.transitionType === "generated" || details.transitionType === "keyword" || details.transitionType === "keyword_generated") {
-            chrome.tabs.ungroup(details.tabId)
+            });
+        }
+        else if (details.transitionType === "auto_bookmark" || details.transitionType === "typed" || details.transitionType === "generated" || details.transitionType === "keyword" || details.transitionType === "keyword_generated") {
+            chrome.tabs.ungroup(details.tabId);
         }
     }
-})
+});
